4a02e5d7bca27205378de560a1a58cb1d2dccde2
Joe Dahle <joe.dahle@gmail.com>
2017-03-17 08:37:52 -0700

    feat: Add prompt and promptContainer

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,43 +1,45 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
 var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
-  getInitialState: function () {
+  getInitialState: function(){
     return {
       username: ''
     }
   },
-  handleSubmitUser: function (e) {
+  handleUpdateUser: function(e){
+    this.setState({
+      username: e.target.value
+    })
+  },
+  handleSubmitUser: function(e){
     e.preventDefault();
     var username = this.state.username;
     this.setState({
       username: ''
     });
 
-    if (this.props.routeParams.playerOne) {
-      this.context.router.push({
+    if(this.props.routeParams.playerOne){
+      this.contextTypes.router.push({
         pathname: '/battle',
         query: {
-          playerOne: this.props.routeParams.playerOne,
-          playerTwo: this.state.username,
+          playerOne: this.props.routParams.playerOne,
+          playerTwo: this.state.username
         }
       })
+      // go to /battle
     } else {
       this.context.router.push('/playerTwo/' + this.state.username)
     }
   },
-  handleUpdateUser: function (event) {
-    this.setState({
-      username: event.target.value
-    });
-  },
-  render: function () {
-    return (
-      <Prompt
-        onSubmitUser={this.handleSubmitUser}
+  render: function(){
+    // console.log(this);
+    return(
+      <Prompt onSubmitUser={this.handleSubmitUser}
         onUpdateUser={this.handleUpdateUser}
         header={this.props.route.header}
         username={this.state.username} />

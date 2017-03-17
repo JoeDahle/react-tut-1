0c558bdca0759b9e4fc428227af05781218efd6b
Joe Dahle <joe.dahle@gmail.com>
2017-03-17 09:45:08 -0700

    feat: Add github api support

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,7 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link
 var transparentBg = require('../styles').transparentBg;
 
-function Home () {
+function Home(){
   return (
     <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
       <h1>Github Battle</h1>

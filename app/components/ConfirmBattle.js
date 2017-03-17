2e260552f92d7d2d50a41564b86abec06c9e698c
Joe Dahle <joe.dahle@gmail.com>
2017-03-17 10:30:48 -0700

    style: Update confirm battle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,9 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('../components/UserDetails');
+var UserDetailsWrapper = require('../components/UserDetailsWrapper');
 
 function puke(obj){
   return <pre>{JSON.stringify(obj, null, " ")}</pre>
@@ -7,7 +12,37 @@ function puke(obj){
 function ConfirmBattle(props){
   return props.isLoading === true
     ? <p>Loading</p>
-    : <div>Confirm Battle: {puke(props)}</div>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <UserDetailsWrapper header="Player 1">
+            <UserDetails info={props.playerInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header="Player 2">
+            <UserDetails info={props.playerInfo[1]} />
+          </UserDetailsWrapper>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+              Initite Battle
+            </button>
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            <Link to="/playerOne">
+              <button type="button" className="btn btn-lg btn-danger">
+                Reselect Players
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.PropTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playerinfo: PropTypes.array.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired
 }
 
 module.exports = ConfirmBattle;
